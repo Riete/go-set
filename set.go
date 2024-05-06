e57104dd5cd85e610be38a34b51385fd90cba3a9
riet <loli_520@126.com>
2024-05-06 14:41:26 +0800

    generic set

--- set.go
+++ set.go
@@ -2,34 +2,180 @@ package set
 
 import (
 	"context"
+	"sync"
 )
 
-type Set interface {
-	Add(i ...interface{})
-	Remove(i ...interface{})
-	Clear()
-	Count() int
-	Has(i ...interface{}) bool
-	Iter(context.Context) <-chan interface{}
-	SubsetOf(Set) bool
-	HasSubset(Set) bool
-	Equal(Set) bool
-	Copy() Set
-	Union(Set) Set
-	Intersect(Set) Set
-	Diff(Set) Set
-	MirrorDiff(Set) Set
-	All() []interface{}
+type Set[T comparable] struct {
+	s  map[T]struct{}
+	rw sync.RWMutex
 }
 
-func NewSet(items ...interface{}) Set {
-	s := newSafeSet()
+func New[T comparable](items ...T) *Set[T] {
+	s := &Set[T]{s: make(map[T]struct{})}
 	s.Add(items...)
 	return s
 }
 
-func NewThreadUnsafeSet(items ...interface{}) Set {
-	s := newUnsafeSet()
-	s.Add(items...)
-	return s
+func (s *Set[T]) Add(items ...T) {
+	s.rw.Lock()
+	defer s.rw.Unlock()
+	for _, item := range items {
+		s.s[item] = struct{}{}
+	}
+}
+
+func (s *Set[T]) Remove(items ...T) {
+	s.rw.Lock()
+	defer s.rw.Unlock()
+	for _, item := range items {
+		delete(s.s, item)
+	}
+}
+
+func (s *Set[T]) Clear() {
+	s.rw.Lock()
+	defer s.rw.Unlock()
+	s.s = make(map[T]struct{})
+}
+
+func (s *Set[T]) Has(items ...T) bool {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	for _, item := range items {
+		if _, found := s.s[item]; !found {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Set[T]) Iter(ctx context.Context) <-chan T {
+	ch := make(chan T)
+	go func() {
+		s.rw.RLock()
+		defer s.rw.RUnlock()
+		defer close(ch)
+		for key := range s.s {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				ch <- key
+			}
+		}
+	}()
+	return ch
+}
+
+func (s *Set[T]) Count() int {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	return len(s.s)
+}
+
+func (s *Set[T]) SubsetOf(other *Set[T]) bool {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	if s.Count() > other.Count() {
+		return false
+	}
+	for key := range s.s {
+		if !other.Has(key) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Set[T]) HasSubset(other *Set[T]) bool {
+	return other.SubsetOf(s)
+}
+
+func (s *Set[T]) Equal(other *Set[T]) bool {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	if s.Count() != other.Count() {
+		return false
+	}
+	for key := range s.s {
+		if !other.Has(key) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Set[T]) Copy() *Set[T] {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	set := New[T]()
+	for key := range s.s {
+		set.Add(key)
+	}
+	return set
+}
+
+func (s *Set[T]) Union(other *Set[T]) *Set[T] {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	unionSet := New[T]()
+
+	for key := range s.s {
+		unionSet.Add(key)
+	}
+	for key := range other.s {
+		unionSet.Add(key)
+	}
+	return unionSet
+}
+
+func (s *Set[T]) Intersect(other *Set[T]) *Set[T] {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	intersectSet := New[T]()
+	if s.Count() > other.Count() {
+		for key := range s.s {
+			if other.Has(key) {
+				intersectSet.Add(key)
+			}
+		}
+	} else {
+		for key := range other.s {
+			if s.Has(key) {
+				intersectSet.Add(key)
+			}
+		}
+	}
+	return intersectSet
+}
+
+func (s *Set[T]) Diff(other *Set[T]) *Set[T] {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	diffSet := New[T]()
+	for key := range s.s {
+		if !other.Has(key) {
+			diffSet.Add(key)
+		}
+	}
+	return diffSet
+}
+
+func (s *Set[T]) MirrorDiff(other *Set[T]) *Set[T] {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	mirrorDiffSet := New[T]()
+	mirrorDiffSet.Add(s.Diff(other).All()...)
+	mirrorDiffSet.Add(other.Diff(s).All()...)
+	return mirrorDiffSet
+}
+
+func (s *Set[T]) All() []T {
+	s.rw.RLock()
+	defer s.rw.RUnlock()
+	var items []T
+	for key := range s.s {
+		items = append(items, key)
+	}
+	return items
 }
--- set_test.go
+++ set_test.go
@@ -7,45 +7,32 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	s := NewSet()
-	us := NewThreadUnsafeSet()
+	s := New(1, 2)
 	s.Add(1, 2, 3)
-	us.Add(1, 2, 3)
 	t.Log(s.All())
-	t.Log(us.All())
 }
 
 func TestRemove(t *testing.T) {
-	s := NewSet(1, 2, 3)
-	us := NewThreadUnsafeSet(1, 2, 4)
+	s := New(1, 2, 3)
 	s.Remove(1, 2)
-	us.Remove(2, 4, 3)
 	t.Log(s.All())
-	t.Log(us.All())
 }
 
 func TestClear(t *testing.T) {
-	s := NewSet(1, 2, 3)
-	us := NewThreadUnsafeSet(1, 2, 4)
+	s := New(1, 2, 3)
 	s.Clear()
-	us.Clear()
 	t.Log(s.All())
-	t.Log(us.All())
 }
 
 func TestHas(t *testing.T) {
-	s := NewSet(1, 2, 3)
-	us := NewThreadUnsafeSet(1, 2, 4)
+	s := New(1, 2, 3)
 	t.Log(s.Has(1, 2, 3), s.Has(2, 3, 4))
-	t.Log(us.Has(1, 2, 4), us.Has(1, 2, 9))
 }
 
 func TestIter(t *testing.T) {
 	ctx1, cancel1 := context.WithCancel(context.Background())
-	ctx2, cancel2 := context.WithCancel(context.Background())
 
-	s := NewSet(1, 2, 3)
-	us := NewThreadUnsafeSet(1, 2, 3)
+	s := New(1, 2, 3)
 	go func() {
 		time.Sleep(2 * time.Second)
 		cancel1()
@@ -54,90 +41,64 @@ func TestIter(t *testing.T) {
 		time.Sleep(2 * time.Second)
 		t.Log("safe set:", i)
 	}
-	go func() {
-		time.Sleep(2 * time.Second)
-		cancel2()
-	}()
-	for i := range us.Iter(ctx2) {
-		time.Sleep(2 * time.Second)
-		t.Log("unsafe set:", i)
-	}
 }
 
 func TestCount(t *testing.T) {
-	s := NewSet(1, 2, 3)
-	us := NewThreadUnsafeSet(1, 2, 3)
-	t.Log(s.Count(), us.Count())
+	s := New(1, 2, 3)
+	t.Log(s.Count())
 }
 
 func TestSubsetOf(t *testing.T) {
-	s := NewSet(true, 1, 2, 3)
-	o1 := NewSet(true, 1, 2, 3, 4)
-	o2 := NewSet(true, 2, 3, 4)
+	s := New(1, 2, 3)
+	o1 := New(1, 2, 3, 4)
+	o2 := New(2, 3, 4)
 
-	us := NewThreadUnsafeSet(false, 1, 2, 3)
-	uo1 := NewThreadUnsafeSet(false, 1, 2, 3, 4)
-	uo2 := NewThreadUnsafeSet(false, 2, 3, 4)
 	t.Log(s.SubsetOf(o1), s.SubsetOf(o2))
-	t.Log(us.SubsetOf(uo1), us.SubsetOf(uo2))
 }
 
 func TestHasSubset(t *testing.T) {
-	s := NewSet(true, 1, 2, 3, 4, 5)
-	o1 := NewSet(true, 1, 2, 3, 4)
-	o2 := NewSet(true, 2, 3, 4, 6)
+	s := New(1, 2, 3, 4, 5)
+	o1 := New(1, 2, 3, 4)
+	o2 := New(2, 3, 4, 6)
 
-	us := NewThreadUnsafeSet(false, 1, 2, 3, 5, 4)
-	uo1 := NewThreadUnsafeSet(false, 1, 2, 3, 4)
-	uo2 := NewThreadUnsafeSet(false, 2, 3, 4, 6)
 	t.Log(s.HasSubset(o1), s.HasSubset(o2))
-	t.Log(us.HasSubset(uo1), us.HasSubset(uo2))
 }
 
 func TestEqual(t *testing.T) {
-	s := NewSet(true, 1, 2, 3, 4, 5)
-	s1 := NewSet(true, 1, 2, 3, 4, 5)
-	s2 := NewSet(true, 1, 2, 3, 4)
+	s := New(1, 2, 3, 4, 5)
+	s1 := New(1, 2, 3, 4, 5)
+	s2 := New(1, 2, 3, 4)
 
-	us := NewThreadUnsafeSet(false, 1, 2, 3, 5, 4)
-	us1 := NewThreadUnsafeSet(false, 1, 2, 3, 5, 4)
-	us2 := NewThreadUnsafeSet(false, 1, 2, 3, 5)
 	t.Log(s.Equal(s1), s.Equal(s2))
-	t.Log(us.Equal(us1), us.Equal(us2))
+}
+
+func TestCopy(t *testing.T) {
+	s := New("a", "b", "c")
+	c := s.Copy()
+	s.Remove("a", "b")
+	t.Log(s.All(), c.All())
 }
 
 func TestUnion(t *testing.T) {
-	s := NewSet(true, 1, 2)
-	s1 := NewSet(true, "a", "b", 1, 2)
-	us := NewThreadUnsafeSet(false, 1, 2)
-	us1 := NewThreadUnsafeSet(false, "a", "b", 1, 2)
+	s := New("a", "b")
+	s1 := New("c", "d")
 	t.Log(s.Union(s1).All())
-	t.Log(us.Union(us1).All())
 }
 
 func TestIntersect(t *testing.T) {
-	s := NewSet(true, 1, 2)
-	s1 := NewSet(true, "a", "b", 1, 2)
-	us := NewThreadUnsafeSet(false, 1, 2)
-	us1 := NewThreadUnsafeSet(false, "a", "b", 1, 2)
+	s := New("aa", "b")
+	s1 := New("a", "b")
 	t.Log(s.Intersect(s1).All())
-	t.Log(us.Intersect(us1).All())
 }
 
 func TestDiff(t *testing.T) {
-	s := NewSet(true, 1, 2, 3)
-	s1 := NewSet(true, "a", "b", 1, 2)
-	us := NewThreadUnsafeSet(false, 1, 2, 3)
-	us1 := NewThreadUnsafeSet(false, "a", "b", 1, 2)
+	s := New(1, 2, 3)
+	s1 := New(1, 2)
 	t.Log(s.Diff(s1).All(), s1.Diff(s).All())
-	t.Log(us.Diff(us1).All(), us1.Diff(us).All())
 }
 
 func TestMirrorDiff(t *testing.T) {
-	s := NewSet(true, 1, 2, 3)
-	s1 := NewSet(true, "a", "b", 1, 2)
-	us := NewThreadUnsafeSet(false, 1, 2, 3)
-	us1 := NewThreadUnsafeSet(false, "a", "b", 1, 2)
+	s := New(1, 2, 3)
+	s1 := New(1, 2, 5)
 	t.Log(s.MirrorDiff(s1).All())
-	t.Log(us.MirrorDiff(us1).All())
 }
